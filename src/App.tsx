053044343ddc90dbbe2062ad51afaec99272072d
chemagaton24 <chembeeragaton@gmail.com>
2023-02-11 20:24:43 +0800

    cxcx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and sssasdsve to reload.
+          Edit <code>src/App.tsx</code> and asdsve to recxcxload.
         </p>
         <a
           className="App-link"
